dd387405d7a71d7aadb0652d02991727d621ab5b
jdjd1114 <2120150362@mail.nankai.edu.cn>
2018-01-16 22:29:44 +0800

    tools updated

--- tools/run.sh
+++ tools/run.sh
@@ -2,17 +2,19 @@
 dataset=$1
 
 acc=""
+time=""
 output=`echo $1 | cut -d . -f 1`
 output=${output}"_results.csv"
 for j in $(seq 1 $2)
 do
-    ./bin/gcn $dataset 2 0 > log.txt
+    ./bin/gcn $dataset > log.txt
     echo -n $j >> $output
     echo -n " " >> $output
-    acc=$(grep Accuracy log.txt | cut -d " " -f 7,9)
+    acc=$(grep Accuracy log.txt | cut -d " " -f 7,10)
+    time=$(grep Global log.txt | cut -d " " -f 16)
     echo -n $acc >> $output
     echo -n " " >> $output
-    grep Global log.txt | cut -d " " -f 27 >> $output
+    echo $time >> $output
     sleep 5
 done
 
