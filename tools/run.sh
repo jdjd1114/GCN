c1c5cbd2ff344bdeba58bc64ba142c8b739af295
jdjd1114 <2120150362@mail.nankai.edu.cn>
2018-05-02 16:29:14 +0800

    Script adds support for sgd

--- tools/run.sh
+++ tools/run.sh
@@ -12,7 +12,11 @@ do
     echo -n $j >> $output
     echo -n " " >> $output
     acc=$(grep Accuracy log.txt | cut -d " " -f 7,10)
-    time=$(grep Global log.txt | cut -d " " -f 16)
+    if grep MBGD log.txt; then
+        time=$(grep Global log.txt | cut -d " " -f 16)
+    else
+        time=$(grep Global log.txt | cut -d " " -f 12)
+    fi
     echo -n $acc >> $output
     echo -n " " >> $output
     echo $time >> $output
